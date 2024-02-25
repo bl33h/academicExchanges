9572a803a04400ac943e921d527c5b4b6a3201c3
bl33h <per21371@uvg.edu.gt>
2024-02-25 14:06:09 -0600

    bugs fixed

--- src/back/client.py
+++ src/back/client.py
@@ -1,19 +1,18 @@
-import datetime   # This will be needed later
 import os
-
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
-# Load config from a .env file
+# ------- env setup -------
+# load config from a .env file
 load_dotenv()
 MONGODB_URI = os.environ['MONGODB_URI']
 
-# Connect to your MongoDB cluster
+# connect to the MongoDB cluster
 client = MongoClient(MONGODB_URI)
 
 db = client['Intercambios']
 
-# List all the collections
+# list all the collections
 collections = db.list_collection_names()
 for collection in collections:
    print(collection)
