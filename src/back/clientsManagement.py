50b5f56c2a64d9042e61ea6ffb6503e02578035e
bl33h <per21371@uvg.edu.gt>
2024-02-25 16:44:03 -0600

    gets operations done

--- src/back/clientsManagement.py
+++ src/back/clientsManagement.py
@@ -18,17 +18,19 @@ usersRouter = APIRouter()
 studentsRouter = APIRouter()
 countriesRouter = APIRouter()
 
-@studentsRouter.get("/students/")
+@studentsRouter.get("/students/") # y
 async def get_students():
     print (await db["students"].find().to_list(1000))
+    return await db["students"].find().to_list(1000)
 
 @studentsRouter.post("/students/", response_model=Student)
 async def create_student(student: Student):
     new_student = await db["students"].insert_one(student.dict(by_alias=True))
     return await db["students"].find_one({"_id": new_student.inserted_id})
 
-@countriesRouter.get("/countries/")
+@countriesRouter.get("/countries/") # y
 async def get_countries():
+    print (await db["countries"].find().to_list(1000))
     return await db["countries"].find().to_list(1000)
 
 @countriesRouter.post("/countries/", response_model=Country)
@@ -36,9 +38,10 @@ async def create_country(country: Country):
     new_country = await db["countries"].insert_one(country.dict(by_alias=True))
     return await db["countries"].find_one({"_id": new_country.inserted_id})
 
-@majorsRouter.get("/careers/")
+@majorsRouter.get("/careers/") # y
 async def get_careers():
     careers = await db["careers"].find().to_list(1000)
+    print(careers)
     return careers
 
 @majorsRouter.post("/careers/", response_model=Career)
@@ -47,9 +50,10 @@ async def create_career(career: Career):
     created_career = await db["careers"].find_one({"_id": new_career.inserted_id})
     return created_career
 
-@exchangesRouter.get("/exchanges/")
+@exchangesRouter.get("/exchanges/") # y
 async def get_exchanges():
     exchanges = await db["exchanges"].find().to_list(1000)
+    print(exchanges)
     return exchanges
 
 @exchangesRouter.post("/exchanges/", response_model=Exchange)
@@ -58,9 +62,10 @@ async def create_exchange(exchange: Exchange):
     created_exchange = await db["exchanges"].find_one({"_id": new_exchange.inserted_id})
     return created_exchange
 
-@universitiesRouter.get("/universities/")
+@universitiesRouter.get("/universities/") # y
 async def get_universities():
     universities = await db["universities"].find().to_list(1000)
+    print(universities)
     return universities
 
 @universitiesRouter.post("/universities/", response_model=University)
@@ -69,9 +74,10 @@ async def create_university(university: University):
     created_university = await db["universities"].find_one({"_id": new_university.inserted_id})
     return created_university
 
-@usersRouter.get("/users/")
+@usersRouter.get("/users/") # y
 async def get_users():
     users = await db["users"].find().to_list(1000)
+    print(users)
     return users
 
 @usersRouter.post("/users/", response_model=User)
